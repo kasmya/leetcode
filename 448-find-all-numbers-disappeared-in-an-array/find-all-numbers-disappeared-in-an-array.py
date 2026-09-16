4df9099ab5a156b31eb325dc02072d1349193afa
kasmya bhatia <109264892+kasmya@users.noreply.github.com>
2026-09-16 23:18:37 +0530

    Time: 48 ms (16.52%) | Memory: 30.9 MB (52.45%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        
+        # Mark visited numbers
+        for i in range(n):
+            index = abs(nums[i]) - 1
+            if nums[index] > 0:
+                nums[index] = -nums[index]
+        
+        result = []
+        for i in range(n):
+            if nums[i] > 0:
+                result.append(i + 1)
+        
+        return result
